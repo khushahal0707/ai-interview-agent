10722811e4d061cda2079acaea6bb18a34f76d16
apple <apple@apples-MacBook-Air.local>
2026-07-11 01:40:33 +0530

    Updated backend URL

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ import InterviewHistory from './pages/InterviewHistory'
 import Pricing from './pages/Pricing'
 import InterviewReport from './pages/InterviewReport'
 
-export const ServerUrl  = "http://localhost:8000"
+const ServerUrl = "https://ai-interview-backend-gr68.onrender.com";
 
 function App() {
 
